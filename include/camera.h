058fe8eab2b5117ead1439240ca2d4f7c950b3e4
Jie Xiang <n.jiexiang@gmail.com>
2023-12-24 00:33:45 +0800

    Add focal blur effect to camera

--- include/camera.h
+++ include/camera.h
@@ -5,15 +5,16 @@
 
 class Camera {
 public:
-    Camera(int width, int height, float verticalFOV = 25.0f)
-            : width(width), height(height), verticalFOV(verticalFOV) {
-        aspectRatio = static_cast<float>(width) / height;
+    Camera(int imageWidth, int imageHeight, float verticalFOV = 20.0f)
+            : imageWidth(imageWidth), imageHeight(imageHeight), verticalFOV(verticalFOV) {
 
+        aspectRatio = static_cast<float>(imageWidth) / imageHeight;
         cameraCenter = lookFrom;
+
         float theta = verticalFOV * pi / 180.0f; // convert to radians
         float h = std::tan(theta / 2.0f);
 
-        focalLength = glm::length(lookFrom - lookAt);
+        // focalLength = glm::length(lookFrom - lookAt);
         viewportHeight = 2.0f * h * focalLength;
         viewportWidth = viewportHeight * aspectRatio;
 
@@ -24,35 +25,43 @@ public:
         viewportU = viewportWidth * u;
         viewportV = -viewportHeight * v;
 
-        pixelDeltaU = viewportU / static_cast<float>(width);
-        pixelDeltaV = viewportV / static_cast<float>(height);
+        pixelDeltaU = viewportU / static_cast<float>(imageWidth);
+        pixelDeltaV = viewportV / static_cast<float>(imageHeight);
         
         viewportUpperLeft = cameraCenter - (focalLength * w) - viewportU / 2.0f - viewportV / 2.0f;
         pixel00Loc = viewportUpperLeft + 0.5f * (pixelDeltaU + pixelDeltaV);
+
+        theta = defocusAngle * pi / 180.0f;
+        float defocusRadius = focalLength * std::tan(theta / 2.0f);
+        defocusDiskU = u * defocusRadius;
+        defocusDiskV = v * defocusRadius;
     }
 
     Ray generateRay(int i, int j, Sampler* sampler) {
         // TODO: implement better filters to replace current box filter
-        Vec2 filter = sampler->get2D() - Vec2(0.5f, 0.5f);
+        Vec2 filterSample = sampler->get2DPixel() - Vec2(0.5f, 0.5f);
         Vec3 pixelCenter = pixel00Loc + static_cast<float>(i) * pixelDeltaU + static_cast<float>(j) * pixelDeltaV;
+        Vec3 pixelSample = pixelCenter + filterSample.x * pixelDeltaU + filterSample.y * pixelDeltaV;
 
-        pixelCenter += Vec3(filter.x * pixelDeltaU.x, filter.y * pixelDeltaV.y, 0.0f);
-        
-        Vec3 rayDirection = pixelCenter - cameraCenter;
-        return Ray(cameraCenter, rayDirection);
+        Vec2 defocus = sampleUniformDiskConcentric(sampler->get2DPixel());
+        Vec3 rayOrigin = cameraCenter + defocus.x * defocusDiskU + defocus.y * defocusDiskV;
+        Vec3 rayDirection = pixelSample - rayOrigin;
+
+        return Ray(rayOrigin, rayDirection);
     }
 
     // TODO: design a better camera interface
     Vec3 lookFrom = Vec3(-2.0f, 2.0f, 1.0f);
     Vec3 lookAt = Vec3(0.0f, 0.0f, -1.0f);
     Vec3 cameraUp = Vec3(0.0f, 1.0f, 0.0f);
+    float defocusAngle = 10.0f;
+    float focalLength = 3.4f;
 
 private:
-    int width;
-    int height;
+    int imageWidth;
+    int imageHeight;
 
     float aspectRatio;
-    float focalLength;
     float viewportHeight;
     float viewportWidth;
     float verticalFOV;      // vertical field of view in degrees
@@ -65,4 +74,6 @@ private:
     Vec3 viewportUpperLeft;
     Vec3 pixel00Loc;
     Vec3 u, v, w;           // camera frame basis vectors
+    Vec3 defocusDiskU;
+    Vec3 defocusDiskV;
 };
